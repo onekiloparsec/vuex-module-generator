4ceca2338be3d8ffa12e54e00a73899c91e4ea9d
Cédric Foellmi <cedric@onekilopars.ec>
2018-08-30 12:18:08 +0200

    List must use no uuid but params

--- src/moduleGenerator.js
+++ src/moduleGenerator.js
@@ -107,7 +107,7 @@ const createMutationSuccesses = (listName, selectionName, singleSelectionName, i
 })
 
 const createApiActions = (api, idKey, dataKey) => ({
-  list: (obj) => api.get(obj), // obj is assumed to be an object. Used a URL parameters.
+  list: (obj) => api.get(null, obj), // obj is assumed to be an object. Used a URL parameters.
   create: (obj) => {
     // Here the presence of TREE_PARENT_ID decides whether one add a child to a tree, or simply an item to a list
     if (_.isNil(obj[TREE_PARENT_ID])) {
