c311d12b73096ba721e11f1d5539aa7aac3e6cbf
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-09-11 09:22:33 +0200

    Fixing tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/ActionsLists.spec.js
+++ tests/ActionsLists.spec.js
@@ -57,7 +57,7 @@ describe('test async api actions on module directly', () => {
       apiURL: API_URL,
       apiPath: 'items/',
       root: 'item',
-      idKey: 'uuid',
+      idKey: 'id',
       lcrusd: 'lcrusd',
       allowMultipleSelection: false,
       allowTree: false
@@ -103,17 +103,20 @@ describe('test async list & read api mutations inside a valid store', () => {
       apiURL: API_URL,
       apiPath: 'items/',
       root: 'item',
-      idKey: 'uuid',
+      idKey: 'id',
       lcrusd: 'lcrusd',
       allowMultipleSelection: false,
       allowTree: false
     })
 
     mutations = {
+      ...itemsModule.mutations,
       listItemsPending: jest.fn(),
       listItemsSuccess: jest.fn(),
       readItemPending: jest.fn(),
-      readItemSuccess: jest.fn()
+      readItemSuccess: jest.fn(),
+      deleteItemPending: jest.fn(),
+      deleteItemSuccess: jest.fn()
     }
 
     store = new Vuex.Store({ modules: { items: { ...itemsModule, mutations: mutations } } })
@@ -134,6 +137,48 @@ describe('test async list & read api mutations inside a valid store', () => {
     await store.dispatch('items/readItem', 3)
     expect(mutations.readItemPending).toHaveBeenCalledWith(expect.any(Object), 3)
     expect(mutations.readItemSuccess).toHaveBeenCalledWith(expect.any(Object), mock3)
+    done()
+  })
+
+  test('deleting an item', async done => {
+    // Check at top of file for mocked requests
+    await store.dispatch('items/deleteItem', 2)
+    expect(mutations.deleteItemPending).toHaveBeenCalledWith(expect.any(Object), 2)
+    expect(mutations.deleteItemSuccess).toHaveBeenCalledWith(expect.any(Object), null)
+    done()
+  })
+})
+
+// Tests at the store level
+
+describe('test async update actions inside a valid store', () => {
+  let store
+
+  beforeEach(() => {
+    let itemsModule = makeModule({
+      http: Vue.http,
+      apiURL: API_URL,
+      apiPath: 'items/',
+      root: 'item',
+      idKey: 'id',
+      lcrusd: 'lcrusd',
+      allowMultipleSelection: false,
+      allowTree: false
+    })
+
+    store = new Vuex.Store({ modules: { items: itemsModule } })
+  })
+
+  afterEach(() => {
+    store = null
+  })
+
+  test('deleting an item', async done => {
+    // Check at top of file for mocked requests
+    await store.commit('items/updateItemsList', [mock1, mock2, mock3])
+    expect(store.state.items.items).toEqual([mock1, mock2, mock3])
+    await store.dispatch('items/deleteItem', 2)
+    expect(store.state.items.items).toEqual([mock1, mock3])
     done()
   })
 })
