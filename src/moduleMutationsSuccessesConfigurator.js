abf9540df1c4f342fd1cbbcee4f2d791de3b822c
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-05-16 11:01:07 +0200

    Better (sinpler ) mutations succesed
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/moduleMutationsSuccessesConfigurator.js
+++ src/moduleMutationsSuccessesConfigurator.js
@@ -2,18 +2,16 @@ import Vue from 'vue'
 import concat from 'lodash/concat'
 import includes from 'lodash/includes'
 
-import { mutationsSuccessRUD } from './utils'
-
 // idKey is a string such as 'pk', or 'uuid' or 'identifier' etc.
-export const configureSuccessMutations = (listName, selectionName, singleSelectionName, idKey) => ({
+export const configureSuccessMutations = (listName, selectionsKey, selectionKey, idKey) => ({
   list: (state, itemsList) => {
     // list is list, no need of idOrData here.
     state[listName] = itemsList
     // filter out items that are not known anymore. do not use IDs! objects may have changed
-    state[selectionName] = state[selectionName].filter(item => includes(itemsList, item))
+    state[selectionsKey] = state[selectionsKey].filter(item => includes(itemsList, item))
     // also clear single selection if necessary
-    if (itemsList && includes(itemsList, state[singleSelectionName]) === false) {
-      state[singleSelectionName] = null
+    if (itemsList && includes(itemsList, state[selectionKey]) === false) {
+      state[selectionKey] = null
     }
   },
 
@@ -21,10 +19,10 @@ export const configureSuccessMutations = (listName, selectionName, singleSelecti
     // Append new items inside list
     state[listName].push(...itemsList)
     // filter out items that are not known anymore. do not use IDs! objects may have changed
-    state[selectionName] = state[selectionName].filter(item => includes(itemsList, item))
+    state[selectionsKey] = state[selectionsKey].filter(item => includes(itemsList, item))
     // also clear single selection if necessary
-    if (includes(state[listName], state[singleSelectionName]) === false) {
-      state[singleSelectionName] = null
+    if (includes(state[listName], state[selectionKey]) === false) {
+      state[selectionKey] = null
     }
   },
 
@@ -35,99 +33,59 @@ export const configureSuccessMutations = (listName, selectionName, singleSelecti
 
   // READ mutation will update the object if it exists already, or push it inside list if not yet present.
   read: (state, obj) => {
-    const {
-      listCursor,
-      selectionCursor,
-      updateSingleSelection
-    } = mutationsSuccessRUD(state[listName], state[selectionName], state[singleSelectionName], idKey, obj[idKey])
-
-    if (listCursor) {
-      listCursor.list.splice(listCursor.index, 1, obj)
+    const objID = obj[idKey]
+    const index = state[listName].findIndex(item => item[idKey] === objID)
+    if (index > -1) {
+      state[listName].splice(index, 1, obj)
       Vue.set(state, listName, new Array(...state[listName]))
     } else {
       state[listName].push(obj)
-    }
-
-    if (selectionCursor) {
-      selectionCursor.list.splice(selectionCursor.index, 1, obj)
-      Vue.set(state, selectionName, new Array(...state[selectionName]))
-    }
-
-    if (updateSingleSelection) {
-      state[singleSelectionName] = obj
     }
   },
 
   // UPDATE mutation will do the same as READ
   update: (state, obj) => {
-    const {
-      listCursor,
-      selectionCursor,
-      updateSingleSelection
-    } = mutationsSuccessRUD(state[listName], state[selectionName], state[singleSelectionName], idKey, obj[idKey])
-
-    if (listCursor) {
-      listCursor.list.splice(listCursor.index, 1, obj)
+    const objID = obj[idKey]
+    const index = state[listName].findIndex(item => item[idKey] === objID)
+    if (index > -1) {
+      state[listName].splice(index, 1, obj)
       Vue.set(state, listName, new Array(...state[listName]))
     } else {
       state[listName].push(obj)
     }
-
-    if (selectionCursor) {
-      selectionCursor.list.splice(selectionCursor.index, 1, obj)
-      Vue.set(state, selectionName, new Array(...state[selectionName]))
-    }
-
-    if (updateSingleSelection) {
-      state[singleSelectionName] = obj
-    }
+    // Object being used by reference shouldn't require to update selection(s) container & pointer.
   },
 
   // SWAP mutation will do the same as READ
   swap: (state, obj) => {
-    const {
-      listCursor,
-      selectionCursor,
-      updateSingleSelection
-    } = mutationsSuccessRUD(state[listName], state[selectionName], state[singleSelectionName], idKey, obj[idKey])
-
-    if (listCursor) {
-      listCursor.list.splice(listCursor.index, 1, obj)
+    const objID = obj[idKey]
+    const index = state[listName].findIndex(item => item[idKey] === objID)
+    if (index > -1) {
+      state[listName].splice(index, 1, obj)
       Vue.set(state, listName, new Array(...state[listName]))
     } else {
       state[listName].push(obj)
     }
-
-    if (selectionCursor) {
-      selectionCursor.list.splice(selectionCursor.index, 1, obj)
-      Vue.set(state, selectionName, new Array(...state[selectionName]))
-    }
-
-    if (updateSingleSelection) {
-      state[singleSelectionName] = obj
-    }
+    // Object being used by reference shouldn't require to update selection(s) container & pointer.
   },
 
-  // DELETE mutation will...
+  // DELETE mutation will remove object from list container, (multiple) list selections container
+  // and single selection.
   delete: (state, objID) => {
-    const {
-      listCursor,
-      selectionCursor,
-      updateSingleSelection
-    } = mutationsSuccessRUD(state[listName], state[selectionName], state[singleSelectionName], idKey, objID)
+    const listIndex = state[listName].findIndex(item => item[idKey] === objID)
+    const selectionsIndex = state[selectionsKey].findIndex(item => item[idKey] === objID)
 
-    if (listCursor) {
-      listCursor.list.splice(listCursor.index, 1)
+    if (listIndex > -1) {
+      state[listName].splice(listIndex, 1)
       Vue.set(state, listName, new Array(...state[listName]))
     }
-
-    if (selectionCursor) {
-      selectionCursor.list.splice(selectionCursor.index, 1)
-      Vue.set(state, selectionName, new Array(...state[selectionName]))
+    if (selectionsIndex > -1) {
+      state[selectionsKey].splice(selectionsIndex, 1)
+      Vue.set(state, selectionsKey, new Array(...state[selectionsKey]))
     }
 
-    if (updateSingleSelection) {
-      state[singleSelectionName] = null
+    if (state[selectionKey][idKey] === objID) {
+      state[selectionKey] = null
     }
   }
 })
--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,3 @@
-import isNil from 'lodash/isNil'
-import findIndex from 'lodash/findIndex'
-
-const findObjInList = (itemsList, idKey, objId) => {
-  const index = findIndex(itemsList, item => item[idKey] === objId)
-  return (index > -1) ? { list: itemsList, index: index } : null
-}
-
-
-export const mutationsSuccessRUD = (itemsList, selectionList, singleSelection, idKey, idOrObj) => {
-  const objID = (typeof idOrObj === 'object') ? idOrObj[idKey] : idOrObj
-  const listCursor = findObjInList(itemsList, idKey, objID)
-  const selectionCursor = findObjInList(selectionList, idKey, objID)
-  const updateSingleSelection = (!isNil(singleSelection) && singleSelection[idKey] === objID)
-  return { listCursor, selectionCursor, updateSingleSelection }
-}
-
 export const capitalizeFirstChar = str => str.charAt(0).toUpperCase() + str.substring(1)
 
 export const pluralize = (singular) => {
