421dfd6f714ff01da67894dcf5b9f484e3c6de49
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-05-16 10:35:22 +0200

    Not needed
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/storeActions.js
+++
@@ -1,9 +0,0 @@
-import { makeDefaultFetchPromise, makePagedFetchPromise } from './fetchPromisesMaker'
-
-export const makeDefaultStoreAction = (mutationName, crudRequest) => ({ commit }, idOrData) => {
-  return makeDefaultFetchPromise(mutationName, crudRequest, commit, idOrData)
-}
-
-export const makePagedStoreAction = (mutationName, crudRequest) => ({ commit }, idOrData) => {
-  return makePagedFetchPromise(mutationName, crudRequest, commit, idOrData)
-}
