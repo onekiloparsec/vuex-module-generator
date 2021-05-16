9c96c956c13d6557d16fc7a730d99b56aad7c13f
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-05-16 14:22:12 +0200

    Adding tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

---
+++ tests/moduleNamesCreator.spec.js
@@ -0,0 +1,41 @@
+import { createModuleNames } from '@/moduleNamesCreator'
+
+
+describe('test moduleNamesCreator', () => {
+
+  test('state names for items', () => {
+    const items = createModuleNames('item')
+    expect(items).not.toBeNull()
+    expect(items.state.list).toEqual('items')
+    expect(items.state.dataMap).toEqual('itemsDataMap')
+    expect(items.state.status).toEqual('itemsLoadingStatus')
+    expect(items.state.selection).toEqual('selectedItem')
+    expect(items.state.selections).toEqual('selectedItems')
+    expect(items.state.pageCurrent).toEqual('currentItemsPage')
+    expect(items.state.pageTotal).toEqual('totalItemsPageCount')
+  })
+
+  test('mutations names for items', () => {
+    const items = createModuleNames('item')
+    expect(items.mutations.crud.list).toEqual('listItems')
+    expect(items.mutations.crud.create).toEqual('createItem')
+    expect(items.mutations.crud.read).toEqual('readItem')
+    expect(items.mutations.crud.update).toEqual('updateItem')
+    expect(items.mutations.crud.swap).toEqual('swapItem')
+    expect(items.mutations.crud.delete).toEqual('deleteItem')
+    expect(items.mutations.select).toEqual('selectItem')
+    expect(items.mutations.selectMultiple).toEqual('selectMultipleItems')
+    expect(items.mutations.clearSelection).toEqual('clearItemsSelection')
+    expect(items.mutations.updateList).toEqual('updateItemsList')
+  })
+
+  test('actions names for items', () => {
+    const items = createModuleNames('item')
+    expect(items.actions.list).toEqual('listItems')
+    expect(items.actions.create).toEqual('createItem')
+    expect(items.actions.read).toEqual('readItem')
+    expect(items.actions.update).toEqual('updateItem')
+    expect(items.actions.swap).toEqual('swapItem')
+    expect(items.actions.delete).toEqual('deleteItem')
+  })
+})
