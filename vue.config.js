29aa58f17e5f8b24628b65f275fc2627f1894e95
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-07-24 11:16:45 +0200

    Back to old config
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,14 @@ module.exports = {
   productionSourceMap: false,
   configureWebpack: config => {
     config.mode = 'production'
-    config.optimization = { usedExports: true }
-    config.externals = { vuex: 'vuex' }
-    config.module.rule(/\.tsx?$/).use('ts-loader')
-    config.module.rule(/\.js$/).use('source-map-loader')
+    config.optimization = {
+      usedExports: true
+    }
     // plugins: [
     //   new BundleAnalyzerPlugin({ analyzerPort: 8787 })
     // ],
+    config.externals = {
+      vuex: 'vuex'
+    }
   }
 }
