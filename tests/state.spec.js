351d5daa4ddc9c895956f359ef73156ae362c651
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-07-24 13:21:15 +0200

    Fixing test
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/state.spec.js
+++ tests/state.spec.js
@@ -4,6 +4,7 @@ describe('test getStateObject', () => {
   test('state initial properties for items', () => {
     const items = getStateObject('item')
     expect(items).not.toBeNull()
+
     expect(items.items).toEqual([])
     expect(items.itemsDataMap).toEqual({})
     expect(items.itemsLoadingStatus).toEqual({
@@ -15,9 +16,10 @@ describe('test getStateObject', () => {
       delete: null
     })
     expect(items.selectedItem).toEqual(null)
-    expect(items.selectedItems).toEqual([])
     expect(items.currentItemsPage).toEqual(-1)
     expect(items.totalItemsPageCount).toEqual(-1)
     expect(items.lastItemsError).toEqual(null)
+
+    expect(items.selectedItems).toBeUndefined()
   })
 })
