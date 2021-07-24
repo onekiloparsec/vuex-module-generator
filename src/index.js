3db5683b826c8867639ade3e2d1e467c106aabbf
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-07-24 11:21:09 +0200

    Recompilation
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import { getStateObject } from '@/state';
-import { getGettersObject } from '@/getters';
-import { getMutationsObject } from '@/mutations';
-import { buildAPIEndpoint } from '@/endpoints';
-import { getActionsObject } from '@/actions';
+import { getStateObject } from '@/state'
+import { getGettersObject } from '@/getters'
+import { getMutationsObject } from '@/mutations'
+import { buildAPIEndpoint } from '@/endpoints'
+import { getActionsObject } from '@/actions'
 export const makeStoreModule = (basicParams) => {
-    const state = getStateObject(basicParams.rootName);
-    const getters = getGettersObject(basicParams.rootName, basicParams.idKey);
-    const storeModule = { namespaced: true, state, getters };
-    storeModule.attachCustomGetters = (customGetters = {}) => {
-        Object.assign(getters, customGetters || {});
-        return storeModule;
-    };
-    storeModule.generateActions = (actionsParams) => {
-        let _endpoint = buildAPIEndpoint({
-            http: actionsParams.http,
-            baseURL: actionsParams.baseURL,
-            resourcePath: actionsParams.resourcePath,
-            idKey: basicParams.idKey
-        });
-        for (let subPath of actionsParams.subresourcePaths || []) {
-            // @ts-ignore
-            _endpoint = _endpoint.addSubresource(subPath, 'pk');
-        }
-        const multiSelection = basicParams.multiSelection || false;
-        const mutations = getMutationsObject(basicParams.rootName, basicParams.idKey, multiSelection, actionsParams.lcrusd);
-        const actions = getActionsObject(_endpoint, basicParams.rootName, actionsParams.lcrusd, actionsParams.subresourcePaths);
-        Object.assign(storeModule, { mutations, actions, _endpoint });
-        return storeModule;
-    };
-    return storeModule;
-};
-//# sourceMappingURL=index.js.map
+  const state = getStateObject(basicParams.rootName)
+  const getters = getGettersObject(basicParams.rootName, basicParams.idKey)
+  const storeModule = { namespaced: true, state, getters }
+  storeModule.attachCustomGetters = (customGetters = {}) => {
+    Object.assign(getters, customGetters || {})
+    return storeModule
+  }
+  storeModule.generateActions = (actionsParams) => {
+    let _endpoint = buildAPIEndpoint({
+      http: actionsParams.http,
+      baseURL: actionsParams.baseURL,
+      resourcePath: actionsParams.resourcePath,
+      idKey: basicParams.idKey
+    })
+    for (let subPath of actionsParams.subresourcePaths || []) {
+      // @ts-ignore
+      _endpoint = _endpoint.addSubresource(subPath, 'pk')
+    }
+    const multiSelection = basicParams.multiSelection || false
+    const mutations = getMutationsObject(basicParams.rootName, basicParams.idKey, multiSelection, actionsParams.lcrusd)
+    const actions = getActionsObject(_endpoint, basicParams.rootName, actionsParams.lcrusd, actionsParams.subresourcePaths)
+    Object.assign(storeModule, { mutations, actions, _endpoint })
+    return storeModule
+  }
+  return storeModule
+}
+// # sourceMappingURL=index.js.map
