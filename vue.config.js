3f9437765df86def7feb8fc12d73d01224fc79c4
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-07-24 11:35:53 +0200

    Config
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- vue.config.js
+++ vue.config.js
@@ -13,5 +13,12 @@ module.exports = {
     config.externals = {
       vuex: 'vuex'
     }
+    config.module = {
+      rules: [
+        { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+        { test: /\.js$/, loader: 'source-map-loader' },
+        { test: /\.vue$/, loader: 'vue-loader' }
+      ]
+    }
   }
 }
